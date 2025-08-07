232b13b89c882baa88f984d1ae777416d7c96cd7
nitinsehrawat020 <nitinsehrawat020@gmail.com>
2025-08-07 12:45:12 +0530

    ip fixed

--- client/src/common/SummaryApi.js
+++ client/src/common/SummaryApi.js
@@ -1,5 +1,5 @@
 export const BASE_URL =
-  import.meta.env.VITE_SERVER_URL || "http://localhost:5000";
+  import.meta.env.VITE_SERVER_URL || "http://localhost:3000";
 
 const SummaryApi = {
   user: {
