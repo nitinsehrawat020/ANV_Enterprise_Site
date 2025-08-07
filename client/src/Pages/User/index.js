f19e7491a4a6e55544048d339ccc3e0f38f087f0
nitinsehrawat020 <nitinsehrawat020@gmail.com>
2025-08-08 00:43:50 +0530

    revert back changes

--- client/src/Pages/User/index.js
+++ client/src/Pages/User/index.js
@@ -13,16 +13,4 @@ export const Main = styled.main`
   @media ${device.phnAndTab} {
     gap: 1rem;
   }
-
-  /* iPhone 15/16 specific fix - reduce top gap */
-  @media only screen and (device-width: 393px) and (device-height: 852px) and (-webkit-device-pixel-ratio: 3) {
-    gap: 0.5rem;
-    margin-top: -8px;
-  }
-
-  /* iPhone 16 Plus specific fix */
-  @media only screen and (device-width: 430px) and (device-height: 932px) and (-webkit-device-pixel-ratio: 3) {
-    gap: 0.5rem;
-    margin-top: -8px;
-  }
 `;
